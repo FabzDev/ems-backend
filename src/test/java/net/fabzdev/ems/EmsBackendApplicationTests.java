76b5b66b84fd1725a2331ee04df6a9f0f30978be
Fabio <fabioescobarardila@hotmail.com>
2024-08-01 00:35:14 -0500

    First commit: jdbc and jpa implemented

---
+++ src/test/java/net/fabzdev/ems/EmsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package net.fabzdev.ems;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
